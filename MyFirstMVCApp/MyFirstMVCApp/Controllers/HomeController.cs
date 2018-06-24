c522ede73ae2461daea27944d98f508471524dd1
Max <gusstorm132@gmail.com>
2018-06-24 12:32:57 -0700

    results are displaying

--- MyFirstMVCApp/MyFirstMVCApp/Controllers/HomeController.cs
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyFirstMVCApp.Models;
+
 
 namespace MyFirstMVCApp.Controllers
 {
@@ -16,11 +18,20 @@ namespace MyFirstMVCApp.Controllers
             return View();
         }
 
-        //display search results page
+        //display search results page, redirects to Resuilts to stay within single-page-application
         [HttpPost]
         public IActionResult Index(int startYear, int endYear)
         {
-            return View();
+            return RedirectToAction("Results", new{startYear, endYear});
+        }
+
+
+        //shows results based on above
+        public IActionResult Results(int startYear, int endYear)
+        {
+            TimePerson Person = new TimePerson();
+
+            return View( Person.GetPersons(startYear, endYear));
         }
     }
 }
--- MyFirstMVCApp/MyFirstMVCApp/Models/TimePerson.cs
+++ MyFirstMVCApp/MyFirstMVCApp/Models/TimePerson.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MyFirstMVCApp.Models
 {
@@ -14,6 +15,7 @@ namespace MyFirstMVCApp.Models
         public string Country { get; set; }
         public int BirthYear { get; set; }
         public int DeathYear { get; set; }
+        public string Title { get; set; }
         public string Category { get; set; }
         public string Context { get; set; }
 
@@ -25,20 +27,22 @@ namespace MyFirstMVCApp.Models
             ///set a path to somewhere within the environments current file tree
             string path = Environment.CurrentDirectory;
 
-            ///use the previous path to go through the cvs file in the wwwroot folder and assign it to a new path
-            string newPath = path.GetFullPath(path.Combine(path, @"wwwroot\personOfTheYear.cvs"));
+            ///use the previous path to go through the csv file in the wwwroot folder and assign it to a new path
+            string newPath = Path.GetFullPath(Path.Combine(path, @"wwwroot\personOfTheYear.csv"));
 
-            ///take the new path to the cvs file and reads it all into a string array so it can used  in C# objects
+            ///take the new path to the csv file and reads it all into a string array
             string[] myFile = File.ReadAllLines(newPath);
 
-            /// I believe this is where the array of cvs info gets put into the list above (people.Add). 
+            /// This is where the array of csv info gets put into the list above (people.Add). 
             /// it loops over the array, starting at 1 instead of 0 to skip past the headers of each field, 
-            /// and then assigns the info to the appropriate Key's. THe years are tirnerys to account for blanks or 
+            /// and then assigns the info to the appropriate Key's. The years are tirnerys to account for blanks or 
             /// non-ints. 
+            /// //myFile gets split on each comma separator as the csv file uses those to separate things
             /// for each Key in the appropriate fields. it takes the Time Person's fields and adds it to the people
             /// list we made above. 
             for (int i = 1; i < myFile.Length; i++)
 			{
+                string[] fields = myFile[i].Split(',');
                 people.Add(new TimePerson
                 {
                     Year = Convert.ToInt32(fields[0]),
